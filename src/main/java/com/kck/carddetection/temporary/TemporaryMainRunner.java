798e01c79d59f1a43237e1cbdd2a4b1ad6ddbd10
Paweł Mieczyński <mieczynskipawel@gmail.com>
2020-11-22 18:02:09 +0100

    dodanie templategiver

--- src/main/java/com/kck/carddetection/temporary/TemporaryMainRunner.java
+++ src/main/java/com/kck/carddetection/temporary/TemporaryMainRunner.java
@@ -1,5 +1,7 @@
 package com.kck.carddetection.temporary;
 
+import com.kck.carddetection.componnent.CardTemplateGiver;
+import com.kck.carddetection.model.CardSuit;
 import com.kck.carddetection.service.ImageLoader;
 import com.kck.carddetection.service.MatrixProcessor;
 import lombok.RequiredArgsConstructor;
@@ -12,14 +14,17 @@ public class TemporaryMainRunner {
 
     private final ImageLoader imageLoader;
     private final MatrixProcessor matrixProcessor;
-
+    private final CardTemplateGiver cardTemplateGiver;
     //todo to tylko tymczasowa klasa gdzie mozemy sobie testowac w mainie rzeczy
     //todo docelowo zrobi sie jakis serwis ktory w tle bedzie chodzil i czytal zdjecia z jakiegos folderu i wypluwal do innego folderu output
 
     public void main() {
-        Mat mat = imageLoader.loadImage("src/main/resources/Spades.jpg");
+        Mat mat = imageLoader.loadImage("src/main/resources/pojedyncze/1.jpg");
         mat = matrixProcessor.resizeImage(mat, 500, 500);
         mat = matrixProcessor.grayImage(mat);
-        imageLoader.saveImage(mat, "src/main/resources/newImage.jpg");
+        imageLoader.saveImage(mat, "src/main/resources/testImage.jpg");
+
+        imageLoader.saveImage(cardTemplateGiver.getCardSuitMatrix(CardSuit.clubs), "src/main/resources/clubsfortesting.jpg");
+
     }
 }
