6415bd8b85cfacd26d728abe3016855516af9d08
maciej <hercogmaciej@gmai.com>
2020-11-23 09:40:14 +0100

    dodano pewne funkcjonalności do MatrixProcessor, dodano jupytera z prototypem znajdowania kart

--- src/main/java/com/kck/carddetection/services/MatrixProcessor.java
+++ src/main/java/com/kck/carddetection/services/MatrixProcessor.java
@@ -1,7 +1,10 @@
 package com.kck.carddetection.services;
 
+import com.sun.scenario.effect.GaussianBlur;
 import org.bytedeco.opencv.opencv_core.Mat;
+import org.bytedeco.opencv.opencv_core.MatVector;
 import org.bytedeco.opencv.opencv_core.Size;
+import org.bytedeco.opencv.opencv_imgproc.Vec2fVector;
 import org.springframework.stereotype.Service;
 
 import static org.bytedeco.opencv.global.opencv_imgproc.*;
@@ -20,5 +23,14 @@ public class MatrixProcessor {
         resize(imageMatrix, result, new Size(width, height), 45, 45, INTER_CUBIC);
         return result;
     }
-
+    public Mat imageThresholded(Mat imageMatrix){
+        Mat result = new Mat();
+        threshold(imageMatrix, result, 130, 255, CV_THRESH_OTSU );
+        return result;
+    }
+    public Mat bluredImage(Mat imageMatrix){
+        Mat result = new Mat();
+        medianBlur(imageMatrix,result,11);
+        return result;
+    }
 }
--- src/main/java/com/kck/carddetection/temporary/TemporaryMainRunner.java
+++ src/main/java/com/kck/carddetection/temporary/TemporaryMainRunner.java
@@ -4,6 +4,7 @@ import com.kck.carddetection.services.ImageLoader;
 import com.kck.carddetection.services.MatrixProcessor;
 import lombok.RequiredArgsConstructor;
 import org.bytedeco.opencv.opencv_core.Mat;
+import org.bytedeco.opencv.opencv_core.MatVector;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,9 +18,11 @@ public class TemporaryMainRunner {
     //todo docelowo zrobi sie jakis serwis ktory w tle bedzie chodzil i czytal zdjecia z jakiegos folderu i wypluwal do innego folderu output
 
     public void main() {
-        Mat mat = imageLoader.loadImage("src/main/resources/Spades.jpg");
-        mat = matrixProcessor.resizeImage(mat, 500, 500);
+        Mat mat = imageLoader.loadImage("src/main/resources/cardsOnFloor.jpg");
+        mat = matrixProcessor.resizeImage(mat, 600, 400);
         mat = matrixProcessor.grayImage(mat);
+        mat = matrixProcessor.bluredImage(mat);
+        mat = matrixProcessor.imageThresholded(mat);
         imageLoader.saveImage(mat, "src/main/resources/newImage.jpg");
     }
 }
